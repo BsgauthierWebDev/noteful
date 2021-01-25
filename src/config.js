d224bc83cecfbba8ad197ea3776213c15a64aced
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2021-01-24 21:13:24 -0500

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://afternoon-atoll-85452.herokuapp.com/`,
+    API_ENDPOINT: `https://rocky-ocean-37641.herokuapp.com/`,
     API_KEY: process.end.REACT_APP_API_KEY,
   }
